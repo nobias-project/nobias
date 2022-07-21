9c7cf1e5f29019bb56be2944c20847e422df9dca
Carlos Mougan <carmougan@gmail.com>
2022-07-21 06:27:49 -0500

    remove token file

---
+++ tests/test_explanation.py
@@ -0,0 +1,14 @@
+from nobias import ShapEstimator
+
+def test_return_shape():
+    '''
+    Test that the return shape is the same as the input shape.
+    '''
+    from sklearn.ensemble import GradientBoostingRegressor
+    from sklearn.model_selection import cross_val_predict
+    from sklearn.datasets import make_blobs
+    
+    X, y = make_blobs(n_samples=100, centers=3, n_features=5,random_state=0)
+    se = ShapEstimator(model=GradientBoostingRegressor())
+    shap_pred = cross_val_predict(se, X, y, cv=3)
+    assert shap_pred.shape == X.shape
