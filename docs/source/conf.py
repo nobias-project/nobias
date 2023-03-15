f260bc7d19ec063af41793e7f71c7d7951e73b4b
Carlos Mougan <carmougan@gmail.com>
2023-03-15 09:09:41 +0100

    ext

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
-    "myst_parser",
+    "sphinx.ext.myst_parser",
 ]
 
 intersphinx_mapping = {
